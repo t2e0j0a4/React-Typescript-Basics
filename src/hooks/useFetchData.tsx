1f931fa8e73f173c3bbec9671486dea45f24f9cd
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-07-28 00:03:32 +0530

    index signatures

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,24 +1,11 @@
 import { useCallback, useEffect, useState } from 'react'
 
 interface ResponseType {
-    "id": number,
-    "login": string,
-    "node_id": string,
-    "avatar_url": string,
-    "gravatar_id": string,
-    "url": string,
-    "html_url": string,
-    "followers_url": string,
-    "following_url": string,
-    "gists_url": string,
-    "starred_url": string,
-    "subscriptions_url": string,
-    "organizations_url": string,
-    "repos_url": string,
-    "events_url": string,
-    "received_events_url": string,
-    "type": string,
-    "site_admin": boolean
+    id: number,
+    login: string,
+    html_url: string,
+    // Index Signatures
+    [key: string]: string | number,
 }
 
 const useFetchData = (apiEndPoint: string) => {
